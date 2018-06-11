990f2e8d315424ec1122a0fb6d9cf0c8ce925b83
Marc Berli <marc.berli@windowslive.com>
2018-06-11 14:48:43 +0200

    Improve getMissingRoles util

--- src/utils/getMissingRoles.js
+++ src/utils/getMissingRoles.js
@@ -1,7 +1,3 @@
 const getMissingRoles = (shouldHave, has) =>
-  has
-    ? shouldHave
-        .map(should => (has.indexOf(should) >= 0 ? undefined : should)) // if role is missing, add it to missing list
-        .filter(Boolean) // remove undefined entries
-    : shouldHave;
+  has ? shouldHave.filter(should => has.indexOf(should) < 0) : shouldHave;
 export default getMissingRoles;
