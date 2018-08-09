be7e5afbcfc0f609ebf8b939d1d283226eabbfe8
Marc <marc.berli@goldinteractive.ch>
2018-08-09 22:57:12 +0200

    Lint test files

--- __tests__/mapArrayToObj.spec.js
+++ __tests__/mapArrayToObj.spec.js
@@ -1,19 +1,19 @@
-import mapArrayToObj from "../src/utils/mapArrayToObj";
+import mapArrayToObj from '../src/utils/mapArrayToObj';
 
-test("should return an object with array entries as keys", () => {
+test('should return an object with array entries as keys', () => {
   const array = ['a', 'b', 'c'];
   const obj = mapArrayToObj(array);
   const expected = {
-    'a': true,
-    'b': true,
-    'c': true,
-  }
+    a: true,
+    b: true,
+    c: true,
+  };
   expect(obj).toEqual(expected);
 });
 
 test('should return an empty object when an empty array is passed', () => {
   const array = [];
   const obj = mapArrayToObj(array);
-  const expected = {}
+  const expected = {};
   expect(obj).toEqual(expected);
 });
--- __tests__/usage.spec.jsx
+++ __tests__/usage.spec.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { render } from 'react-testing-library'
+import { render } from 'react-testing-library';
 import { AuthProvider, Authorize } from '../src/index';
 
 const roles = ['user', 'admin'];
@@ -10,16 +10,16 @@ test('should render the secured page, if the user has the needed roles', () => {
       <Authorize neededRoles={['user']}>
         {
           ({ isAuthorized }) => {
-            if (isAuthorized) return <span>Secure Text</span>
-            else return <span>Fallback</span>
+            if (isAuthorized) return <span>Secure Text</span>;
+            return <span>Fallback</span>;
           }
         }
       </Authorize>
-    </AuthProvider>
+    </AuthProvider>,
   );
   expect(getByText('Secure Text')).toNotBeNull;
   expect(queryByText('Fallback')).toBeNull;
-})
+});
 
 test('should render the specified fallback if the user lacks some roles', () => {
   const { queryByText } = render(
@@ -27,18 +27,18 @@ test('should render the specified fallback if the user lacks some roles', () => 
       <Authorize neededRoles={['user', 'photographer']}>
         {
           ({ isAuthorized, lacksRole }) => {
-            if (isAuthorized) return <span>Secure Text</span>
-            else if (lacksRole('user')) return <span>Fallback for no User</span>
-            else if (lacksRole('photographer')) return <span>Fallback for no photographer</span>
+            if (isAuthorized) return <span>Secure Text</span>;
+            if (lacksRole('user')) return <span>Fallback for no User</span>;
+            if (lacksRole('photographer')) return <span>Fallback for no photographer</span>;
           }
         }
       </Authorize>
-    </AuthProvider>
+    </AuthProvider>,
   );
   expect(queryByText('Secure Text')).toBeNull;
   expect(queryByText('Fallback for no User')).toBeNull;
   expect(queryByText('Fallback for no photographer')).toNotBeNull;
-})
+});
 
 test('should render the specified fallback if the user has a specific role', () => {
   const { queryByText } = render(
@@ -46,38 +46,38 @@ test('should render the specified fallback if the user has a specific role', () 
       <Authorize neededRoles={['user', 'photographer']}>
         {
           ({ hasRole }) => {
-            if (hasRole('photographer')) return <span>You are an excellent photographer</span>
-            else if (hasRole('user')) return <span>Secret User content</span>
-            else if (hasRole('admin')) return <span>For Admin eyes only</span>
+            if (hasRole('photographer')) return <span>You are an excellent photographer</span>;
+            if (hasRole('user')) return <span>Secret User content</span>;
+            if (hasRole('admin')) return <span>For Admin eyes only</span>;
           }
         }
       </Authorize>
-    </AuthProvider>
+    </AuthProvider>,
   );
   expect(queryByText('You are an excellent photographer')).toBeNull;
   expect(queryByText('For Admin eyes only')).toBeNull;
   expect(queryByText('Secret User content')).toNotBeNull;
-})
+});
 
 beforeEach(() => {
-  jest.spyOn(console, 'error')
-  global.console.error.mockImplementation(() => {})
-})
+  jest.spyOn(console, 'error');
+  global.console.error.mockImplementation(() => {});
+});
 
 afterEach(() => {
-  global.console.error.mockRestore()
-})
+  global.console.error.mockRestore();
+});
 test('incorrect usage', () => {
   const renderWithoutContext = () => render(
     <Authorize neededRoles={['user', 'photographer']}>
       {
         ({ isAuthorized, lacksRole }) => {
-          if (isAuthorized) return <span>Secure Text</span>
-          else if (lacksRole('user')) return <span>Fallback for no User</span>
-          else if (lacksRole('photographer')) return <span>Fallback for no photographer</span>
+          if (isAuthorized) return <span>Secure Text</span>;
+          if (lacksRole('user')) return <span>Fallback for no User</span>;
+          if (lacksRole('photographer')) return <span>Fallback for no photographer</span>;
         }
       }
-    </Authorize>
+    </Authorize>,
   );
-  expect(renderWithoutContext).toThrowError('<Authorize /> used without a corresponding context')
-})
+  expect(renderWithoutContext).toThrowError('<Authorize /> used without a corresponding context');
+});
