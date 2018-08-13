353b93738b06b073d0a39be1750a1c5fdb3d724f
Simon Stucki <simon_stucki@hotmail.com>
2018-08-13 18:38:48 +0200

    Improve code clarity

--- src/AuthProvider.jsx
+++ src/AuthProvider.jsx
@@ -6,11 +6,15 @@ import { Provider } from './AuthContext';
 class AuthProvider extends React.Component {
   static getDerivedStateFromProps(props, state) {
     const { roles } = props;
-    if (state && state.auth.rolesReference === roles) {
+    const isStateSet = !!state;
+    const isSameRolesObjectAsBefore =
+      isStateSet && state.auth.rolesReference === roles;
+    if (isSameRolesObjectAsBefore) {
       return null;
     }
     return {
       auth: {
+        // store a object reference to the original roles array
         rolesReference: roles,
         roles: mapArrayToObj(roles),
       },
